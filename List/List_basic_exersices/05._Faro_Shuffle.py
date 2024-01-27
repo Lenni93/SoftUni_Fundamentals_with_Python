3803ebe5ee59464f562f95a89d52a454edba0099
Lilyana Ivanova <lennibossta@gmail.com>
2024-01-27 19:31:12 +0200

    Update 05._Faro_Shuffle.py

--- List/List_basic_exersices/05._Faro_Shuffle.py
+++ List/List_basic_exersices/05._Faro_Shuffle.py
@@ -2,9 +2,3 @@ shuffle = input().split(",")
 count = int(input())
 middle_of_deck = len(shuffle) // 2
 
-for i in range(count):
-    if count % 3 != 0:
-        list_.append(shuffle)
-        check += list_
-    elif count % 2 == 0:
-
