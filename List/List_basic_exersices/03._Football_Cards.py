f8d16832470a2b3fcb904025f14240a64edf7e07
Lilyana Ivanova <lennibossta@gmail.com>
2024-01-27 19:44:40 +0200

    Update 03._Football_Cards.py

--- List/List_basic_exersices/03._Football_Cards.py
+++ List/List_basic_exersices/03._Football_Cards.py
@@ -8,11 +8,8 @@ for player in players:
     elif player in team_b:
         team_b.remove(player)
     if len(team_a) < 7 or len(team_b) < 7:
-        game_terminated = True
+        game_was_terminated= True
         break
 print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
 if game_was_terminated:
     print("Game was terminated")
-
-
-
