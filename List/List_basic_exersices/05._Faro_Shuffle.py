a03cd005acd29b2378e9206a7b515dbe490efd16
Lilyana Ivanova <lennibossta@gmail.com>
2024-01-28 18:30:18 +0200

    Update 05._Faro_Shuffle.py

--- List/List_basic_exersices/05._Faro_Shuffle.py
+++ List/List_basic_exersices/05._Faro_Shuffle.py
@@ -1,4 +1,13 @@
-shuffle = input().split(",")
+deck_of_cards = input().split()
 count = int(input())
-middle_of_deck = len(shuffle) // 2
 
+for shuffle in range(count):
+    middle_od_deck = len(deck_of_cards) // 2
+    left_deck = deck_of_cards[0:middle_od_deck]
+    right_deck = deck_of_cards[middle_od_deck:]
+    deck_after_shuffle = []
+    for car_index in range(len(left_deck)):
+        deck_after_shuffle.append(left_deck[car_index])
+        deck_after_shuffle.append(right_deck[car_index])
+    deck_of_cards = deck_after_shuffle
+print(deck_of_cards)
