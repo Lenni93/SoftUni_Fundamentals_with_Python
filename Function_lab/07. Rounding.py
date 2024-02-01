ca30d25bfd6c3d30d002b09e5a7809cd7e798dee
Lilyana Ivanova <lennibossta@gmail.com>
2024-02-01 19:05:02 +0200

    Create 07. Rounding.py

---
+++ Function_lab/07. Rounding.py
@@ -0,0 +1,20 @@
+def rounding(num):
+    rounded_numbers = []
+    for number in num:
+        rounded_number = round(number)
+        rounded_numbers.append(rounded_number)
+    return rounded_numbers
+
+
+numbers = [float(nums) for nums in input().split()]
+print(rounding(numbers))
+# def rounding_numbers(num):
+#     rounded_numbers = []
+#     for number in num:
+#         rounded_number = round(number)
+#         rounded_numbers.append(rounded_number)
+#     return rounded_numbers
+#
+#
+# numbers = [float(nums) for nums in input().split()]
+# print(rounding_numbers(numbers))
