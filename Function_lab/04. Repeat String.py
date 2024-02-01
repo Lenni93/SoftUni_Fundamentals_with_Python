28a011b1cdf557244efeb9fd04a8bac7c4237f75
Lilyana Ivanova <lennibossta@gmail.com>
2024-02-01 19:04:52 +0200

    Create 04. Repeat String.py

---
+++ Function_lab/04. Repeat String.py
@@ -0,0 +1,21 @@
+def repeat(a, b):
+    return a * b
+
+
+string = input()
+number_of_repeating = int(input())
+print(repeat(string, number_of_repeating))
+
+
+
+# string = input()
+# n = int(input())
+#
+# repeating_string = lambda a, b: a * b
+# result = repeating_string(string, n)
+# print(result)
+# input
+# aadfs
+# 5
+# output
+# aadfsaadfsaadfsaadfsaadfs
