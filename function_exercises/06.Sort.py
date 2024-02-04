5cf011248e888b2ac9d8c820f8ea378b807141c0
Lilyana Ivanova <lennibossta@gmail.com>
2024-02-04 12:24:25 +0200

    Create 06.Sort.py

---
+++ function_exercises/06.Sort.py
@@ -0,0 +1,9 @@
+def sorting(num):
+    return sorted(num)
+
+
+numbers = [int(number) for number in input().split()]
+print(sorted(numbers))
+
+# Write a program that receives a sequence of numbers (integers) separated by a single space.
+# It should print a sorted list of numbers in ascending order. Use sorted().
