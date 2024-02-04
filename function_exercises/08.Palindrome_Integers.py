87601ccee730c7e05485b84cb22fef31091078ad
Lilyana Ivanova <lennibossta@gmail.com>
2024-02-04 12:24:21 +0200

    Create 08.Palindrome_Integers.py

---
+++ function_exercises/08.Palindrome_Integers.py
@@ -0,0 +1,15 @@
+
+[print(n == n[::-1]) for n in input().split(", ")]
+
+# def check_palindrome(numbers):
+#     [print(n == n[::-1]) for n in numbers]
+#
+# check_palindrome(input().split(", "))
+# # 123, 323, 421, 121
+# Here are some interesting facts about palindromic numbers:
+#
+# The first 30 palindromic numbers (in decimal) are: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11,
+# 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 202, … 1.
+# Palindromic numbers receive attention in recreational mathematics. For instance:
+# Palindromic primes: Examples include 2, 3, 5, 7, 11, 101, 131, 151, … 1.
+# Palindromic square numbers: Examples include 0, 1, 4, 9, 121, 484, 676, 10201, 12321, … 1.
